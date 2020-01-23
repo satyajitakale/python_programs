be83986c60a40516d262c9a24815cc4b2d3b3ced
satyajitakale <60214032+satyajitakale@users.noreply.github.com>
2020-01-23 14:22:34 +0530

    Update single_inheritance.py

--- single_inheritance.py
+++ single_inheritance.py
@@ -3,14 +3,20 @@ class stud:
         self.name=name
         self.prof=prof
         
+    def asch(self):
+        print("I am in stud class\n")
     def stud_d(self):
-        print(f"Name:{self.name}.\ni am a {self.prof}")
+        print(f"Name:{self.name}.\ni am a {self.prof}\n")
 
 class tpo_stud(stud):
+    def __init__(self):
+        pass
     def tpo_stud_d(self,s):
-        print(f"Name:{self.name}.\ni am a{self.prof} and {s} is a tpo student!!")
+        print(f"Name:{self.name}.\ni am a{self.prof} and {s} is a tpo student!!\n")
+    def asch2(self):
+        print("I am in tpo_stud_class\n")
 sat=stud("sat","programmer")
-sattya=tpo_stud("sattya","programmer")
+sattya=tpo_stud()
 sat.stud_d()
-sattya.stud_d()
-sattya.tpo_stud_d("satyam")
+sattya.asch()
+sattya.asch2()
